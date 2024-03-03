07310e7d89103a6ffccf84924dd3439a488a2687
nadamu <maksufo28@gmail.com>
2024-03-03 22:35:35 +0100

    Random message

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+std::string get_message(int len) {
+
+	std::string message;
+	srand(time(NULL));
+
+	for(int i = 0; i < len; i++) {
+		char y = rand();
+		message += y;
+	}
+
+	return message;
+
+}
 
 int main() {
-	std::cout << "Hello 2" << std::endl;
+	int len;
+	std::string message;
+	std::cout << "Podaj dlugosc wiadomosci:" << std::endl;
+	std::cin >> len;
+	message = get_message(len);
+	std::cout << message << std::endl;
 	
 	return 0;
 }
