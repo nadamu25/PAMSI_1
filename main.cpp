7f2a1e719bd155353cc2732c71431d5311a92e4c
nadamu <maksufo28@gmail.com>
2024-03-03 23:27:53 +0100

    dzielenie_wiadomosci_1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 
 
+//uzyskiwanie wiadomosci
 std::string get_message(int len) {
 
 	std::string message;
@@ -15,16 +16,34 @@ std::string get_message(int len) {
 	}
 
 	return message;
-
 }
 
+//struktura pakietu n wiadomosci
+struct package {
+	int n;
+	std::string pack;
+};
+
+
 int main() {
+
+	//uzyskiwanie wiadomosci
 	int len;
 	std::string message;
-	std::cout << "Podaj dlugosc wiadomosci:" << std::endl;
+
+	std::cout << "Podaj dlugosc wiadomosci: ";
 	std::cin >> len;
 	message = get_message(len);
 	std::cout << message << std::endl;
+
+	//dzielenie wiadomosci
+	int package_num;
+	std::cout << "Podaj ilosc pakietow: ";
+	std::cin >> package_num;
+	int package_len = len/package_num;
+	std::cout << package_len;
+
+
 	
 	return 0;
 }
